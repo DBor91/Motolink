1afcd0fbcc3449a1455e544ea472a10b612cd873
DBor91 <damian.borowik01@gmail.com>
2025-06-17 21:58:42 +0200

    refactoring udany

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import register
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path('register/', register, name='register')
 ]
